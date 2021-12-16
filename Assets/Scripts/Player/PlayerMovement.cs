95babdd12a5cb8c5416881f4422f5a602ceb464a
Mrinank S <mrinank.sivakumar@ontariotechu.net>
2021-12-15 22:00:30 -0500

    Minor Fixes

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -12,8 +12,6 @@ public class PlayerMovement : MonoBehaviour
     float turnSmoothVelocity;
 
     private CinemachineFreeLook cinemachineFreeLook;
-    float cameraAngle;
-    float cameraAngleSpeed;
 
     private Joystick movementStick;
     private Joystick cameraStick;
